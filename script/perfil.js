b1c7c86d380da4d8d1c3c12737a78ccb5786511e
ivyesilva08 <mariafernandadasiva0801@gmail.com>
2024-10-09 18:45:32 -0300

    add

--- script/perfil.js
+++ script/perfil.js
@@ -1,1 +1,25 @@
-const modal = document.querySelector("dialog")
+const container = document.getElementById('container-login');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var dialog = document.querySelector("dialog");
+    dialog.showModal(); 
+  
+
+    var closeBtn = document.getElementById('closePopup');
+    closeBtn.addEventListener('click', function() {
+      dialog.close();
+    });
+  });
+   
